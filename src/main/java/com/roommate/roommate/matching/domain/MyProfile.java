6decb1b837a4e110b1834d2efd7aacad55aa155b
Soojin6943 <sjhan0711@gmail.com>
2025-09-01 16:24:35 +0900

    feat: MyProfile 엔티티 추가

--- src/main/java/com/roommate/roommate/matching/domain/MyProfile.java
+++ src/main/java/com/roommate/roommate/matching/domain/MyProfile.java
@@ -2,7 +2,9 @@ package com.roommate.roommate.matching.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.roommate.roommate.auth.domain.User;
+import com.roommate.roommate.matching.enums.*;
 import jakarta.persistence.*;
+import lombok.Cleanup;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,5 +23,57 @@ public class MyProfile {
     @JoinColumn(name = "user_id", nullable = false)
     @JsonIgnore
     private User user;
+
+    // 생활 리듬
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LifeCycle lifeCycle;
+
+    // 흡연
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Smoking smoking;
+
+    // 청소 주기
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CleanFreq cleanFreq;
+
+    // 청결 기준
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TidyLevel tidyLevel;
+
+    // 외부인 방문
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VisitorPolicy visitorPolicy;
+
+    // 욕실 사용 패턴
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RestroomUsagePattern restroomUsagePattern;
+
+    // 음식 냄새
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FoodOdorPolicy foodOdorPolicy;
+
+    // 집에 머무는 시간
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private HomeStay homeStay;
+
+    // 소음 시간
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NoisePreference noisePreference;
+
+    // 수면
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SleepSensitivity sleepSensitivity;
+
+    public void setUser(User user) {this.user = user;}
 
 }
