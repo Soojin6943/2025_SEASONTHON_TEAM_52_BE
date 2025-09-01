cea1ca0c10e03afbcf7d640146296421a15a8e83
kroad0129 <kroad0129@naver.com>
2025-09-01 16:45:50 +0900

    fix: WebClientConfig bean 문제해결

--- src/main/java/com/roommate/roommate/WebClientConfig.java
+++ src/main/java/com/roommate/roommate/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(WebClient.Builder builder){
-        return  builder.build();
+    public WebClient defaultWebClient(WebClient.Builder builder) {
+        return builder.build();
     }
 }
