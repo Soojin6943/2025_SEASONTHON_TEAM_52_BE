6e8ff2474fa318600627651b13fc72106314d7f2
Soojin6943 <sjhan0711@gmail.com>
2025-09-02 23:21:07 +0900

    fix: s3 v1 -> v2

--- src/main/java/com/roommate/roommate/common/s3/S3Uploader.java
+++ src/main/java/com/roommate/roommate/common/s3/S3Uploader.java
@@ -1,13 +1,12 @@
 package com.roommate.roommate.common.s3;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -16,26 +15,29 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3Uploader {
 
-    private final AmazonS3 amazonS3;
+    private final S3Client s3Client;
 
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
+
+    @Value("${spring.cloud.aws.s3.region}")
+    private String region;
 
     public String upload(MultipartFile file) {
         try {
             String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentType(file.getContentType());
-            objectMetadata.setContentLength(file.getSize());
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(fileName)
+                    .contentType(file.getContentType())
+                    .build();
 
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, fileName, file.getInputStream(), objectMetadata);
-            amazonS3.putObject(putObjectRequest);
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
 
-            return String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, amazonS3.getRegionName(), fileName);
+            return String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, region, fileName);
         } catch (IOException e) {
             throw new RuntimeException("파일 업로드 중 오류 발생", e);
         }
     }
-
 }
